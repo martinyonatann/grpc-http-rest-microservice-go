52fb645ee6e714f7d1958104d01111600a7dcc04
NB_Martin <martin.y@inception.asia>
2020-09-26 20:23:08 +0700

    config fcvking structure

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.14
 
 require (
 	github.com/golang/protobuf v1.4.2 // indirect
+	google.golang.org/grpc v1.27.0
 	google.golang.org/grpc/cmd/protoc-gen-go-grpc v0.0.0-20200924182952-21f897eab8af // indirect
 	google.golang.org/protobuf v1.25.0 // indirect
 )
--- pkg/service/v1/todo-service.go
+++ pkg/service/v1/todo-service.go
@@ -39,8 +39,9 @@ func (s *toDoServiceServer) checkAPI (api string) error{
 	return nil
 }
 
+
 //connecct returns SQL database connection from the pool
-func(s *toDoServiceServer) connect (ctx context.Context)(*sql.Conn, err){
+func (s *toDoServiceServer) connect (ctx context.Context)(*sql.Conn, err){
 	c, err := s.db.Conn(ctx)
 	if err != nil{
 		return nil, status.Error(codes.Unknown, "failed to connect to database->"+err.Error())
@@ -48,4 +49,19 @@ func(s *toDoServiceServer) connect (ctx context.Context)(*sql.Conn, err){
 	return c, nil
 }
 
-func(s *toDoServiceServer)
+//Create new todo task
+func (s *toDoServiceServer) Create(ctx context.Context, req *v1.CreateRequest) (*v1.CreateResponse, error) {
+	//Check if the API version requested by client is supported by server
+	if err := s.checkAPI(req.Api); err != nil{
+		return niil, err
+	}
+
+	//get SQL connection from pool
+	c, err := s.connect(ctx)
+	if err != nil{
+		return nil, err
+	}
+	defer c.Close()
+
+	reminder, err := 
+}
